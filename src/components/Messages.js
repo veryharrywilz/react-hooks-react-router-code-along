ebec837d8d78ffec861977ff41031e4ea132ffd2
veryharrywilz <110489443+veryharrywilz@users.noreply.github.com>
2022-10-26 13:11:55 -0400

    pushing with more formatting and routes installed

---
+++ src/components/Messages.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Messages() {
+    return (
+      <div>
+        <h1>You have no new messages</h1>
+      </div>
+    )
+  }
+
+  export default Messages;
---
+++ src/components/NavBar.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+
+const linkStyles = {
+    display: "inline-block",
+    width: "50px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
+
+/* define the NavBar component */
+function NavBar() {
+    return (
+      <div>
+        <NavLink
+          to="/"
+          /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+          exact
+          /* add styling to Navlink */
+          style={linkStyles}
+          /* add prop for activeStyle */
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/login"
+          exact
+          style={linkStyles}
+          activeStyle={{
+            background: "darkblue",
+          }}
+        >
+          Login
+        </NavLink>
+        <NavLink 
+                to="/messages"
+                exact
+                style={linkStyles}
+                activeStyle={{
+                  background: "darkblue",
+                }}>
+          Messages
+          </NavLink>
+      </div>
+    );
+  }
+
+  export default NavBar;
